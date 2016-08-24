cacfbbaf02fdfcb3b091c4d4f212a4104c2b9abc
Joe Steed Mendonca <jsmendonca@gmail.com>
2016-08-24 09:27:20 +0100

    updated webui for aspnet core 1.0

---
+++ src/WebUI/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EasyNetQ;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBus _bus = RabbitHutch.CreateBus("host=rabbit");
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage(string messageTextBox)
+        {
+            _bus.Publish(messageTextBox);
+
+            return View("Sent");
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
