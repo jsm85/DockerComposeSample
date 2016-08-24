9659e0e35b3a3f5737f77fd6afb3707e33a3c4e5
Joe Steed Mendonca <jsmendonca@gmail.com>
2016-08-24 10:55:23 +0100

    upgraded background service to asp.net core 1.0

---
+++ src/BackgroundService/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EasyNetQ;
+
+namespace BackgroundService
+{
+    public class Program
+    {
+        private static readonly ManualResetEvent QuitEvent = new ManualResetEvent(false);
+
+        public static void Main(string[] args)
+        {
+            var bus = RabbitHutch.CreateBus("host=rabbit");
+
+            Console.CancelKeyPress += (sender, eArgs) =>
+            {
+                QuitEvent.Set();
+                eArgs.Cancel = true;
+            };
+
+            bus.Subscribe<string>("sub_id", msg =>
+            {
+                Console.WriteLine(string.Format("Message Received: {0}", msg));
+            });
+
+            QuitEvent.WaitOne();
+
+            bus.Dispose();
+        }
+    }
+}
---
+++ src/BackgroundService/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Walt Disney International")]
+[assembly: AssemblyProduct("BackgroundService")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("66563b34-3f31-40ea-a026-7b0fc9ffdad4")]
